128b597ef246a1cce4888ebcf7bde5d53b6985a4
Unknown <tianying@stat.tamu.edu>
2018-01-23 14:30:53 -0600

    Add package documentation "dap-package"

---
+++ R/packagedocumentation.R
@@ -0,0 +1,11 @@
+#' Discriminant Analysis via Projections 
+#'
+#' This package provides tools for discriminant analysis on binary classification. It contains functions \code{apply_DAP, classify_DAP, cv_DAP, solve_DAP_C, solve_DAP, solve_DAP_seq} for implementing the method Discriminant Analysis via Projections.
+#'
+#' @name dap-package
+#' @docType package
+#' @author Irina Gaynanova and Tianying Wang.
+#'
+#' @keywords package
+#'
+NULL
